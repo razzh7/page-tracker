5290041a716dde8cabc4421d3d0621bb281e9a84
razzh <razzhavenir@163.com>
2023-01-02 14:59:40 +0800

    test: add hash dispatch test

---
+++ test/hash.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, test, vi, beforeEach } from 'vitest'
+import Tracker from '../src/core'
+
+// Mock navigator.sendBeacon
+Object.assign(navigator, {
+  sendBeacon: vi.fn()
+})
+let tracker: any
+const useParams = <T>(eventName: string, targetKey: string, data?: T) => {
+  return {
+    eventName,
+    targetKey,
+    data
+  }
+}
+describe('use historyTracker option', () => {
+  beforeEach(() => {
+    tracker = new Tracker({
+      requestUrl: 'https://localhost:3001',
+      hashTracker: true
+    })
+    vi.spyOn(window.navigator, 'sendBeacon')
+  })
+
+  test('pushState', () => {
+    const reportSpy = vi.spyOn(tracker, 'reportTracker')
+    // 派发 hashchange 事件
+    window.dispatchEvent(
+      new HashChangeEvent('hashchange', {
+        oldURL: 'http://127.0.0.1:5500/index.html#123',
+        newURL: 'http://127.0.0.1:5500/index.html#456'
+      })
+    )
+
+    expect(reportSpy).toHaveBeenCalled()
+    expect(reportSpy).toHaveBeenCalledWith(useParams('hashchange', 'hash-pv'))
+  })
+})
